61fcce6ac04b498c50463edab7fe449da545e48e
Milan Bhandari <milanbhandari@college.harvard.edu>
2021-12-05 01:11:23 -0500

    call market

--- sim.py
+++ sim.py
@@ -5,52 +5,69 @@ import heapq
 from agent import Agent
 
 
-class CDAMarket:
-    @dataclass
-    class BookBid:
-        quantity: int
-        price: float
-        agent_id: int
-        round_num: int
+@dataclass
+class BookBid:
+    quantity: int
+    price: float
+    agent_id: int
+    time: int
 
-        def __lt__(self, other) -> bool:
-            return (self.price, -self.round_num) > (other.price, -other.round_num)
+    def __lt__(self, other: 'BookBid') -> bool:
+        return (self.price, -self.time) > (other.price, -other.time)
 
-    @dataclass
-    class BookAsk:
-        quantity: int
-        price: float
-        agent_id: int
-        round_num: int
 
-        def __lt__(self, other) -> bool:
-            return (self.price, self.round_num) < (other.price, other.round_num)
+@dataclass
+class BookAsk:
+    quantity: int
+    price: float
+    agent_id: int
+    time: int
 
-    @dataclass
-    class Trade:
-        seller_id: int
-        buyer_id: int
-        quantity: int
+    def __lt__(self, other: 'BookAsk') -> bool:
+        return (self.price, self.time) < (other.price, other.time)
 
+
+@dataclass
+class CallTrade:
+    seller_id: int
+    buyer_id: int
+    quantity: int
+
+
+@dataclass
+class CDATrade:
+    seller_id: int
+    buyer_id: int
+    quantity: int
+    price: float
+
+
+class CallMarket:
     def __init__(self, agents: List[Agent]) -> None:
         self.agents = agents
-        self.bids: List[CDAMarket.BookBid] = []
-        self.asks: List[CDAMarket.BookAsk] = []
-        self.round = 0
+        self.bids: List[BookBid] = []
+        self.asks: List[BookAsk] = []
+        self._time: int = 0
 
-    def run_round(self):
+    @property
+    def time(self) -> int:
+        t = self._time
+        self._time += 1
+        return t
+
+    def run(self):
         for agent in self.agents:
-            bids = [CDAMarket.BookBid(b.quantity, b.price, i, self.round)
+            bids = [BookBid(b.quantity, b.price, i, self.time)
                     for i, b in enumerate(agent.bid())]
             for bid in bids:
                 heapq.heappush(self.bids, bid)
 
-            asks = [CDAMarket.BookAsk(b.quantity, b.price, i, self.round)
+            asks = [BookAsk(b.quantity, b.price, i, self.time)
                     for i, b in enumerate(agent.ask())]
             for ask in asks:
                 heapq.heappush(self.asks, ask)
 
-        trades: List[CDAMarket.Trade] = []
+        trades: List[CallTrade] = []
         midpoint_price = 0.0
         while len(self.bids) != 0 and len(self.asks) != 0:
             top_bid = self.bids[0]
@@ -63,18 +80,72 @@ class CDAMarket:
             traded_quantity = abs(top_bid.quantity - top_ask.quantity)
 
             if top_bid.quantity > top_ask.quantity:
-                new_bid = CDAMarket.BookBid(
-                    top_bid.quantity - traded_quantity, top_bid.price, top_bid.agent_id, top_bid.round_num)
+                new_bid = BookBid(
+                    top_bid.quantity - traded_quantity, top_bid.price, top_bid.agent_id, top_bid.time)
                 heapq.heappush(self.bids, new_bid)
             if top_ask.quantity > top_bid.quantity:
-                new_ask = CDAMarket.BookAsk(
-                    top_ask.quantity - traded_quantity, top_ask.price, top_ask.agent_id, top_ask.round_num)
+                new_ask = BookAsk(
+                    top_ask.quantity - traded_quantity, top_ask.price, top_ask.agent_id, top_ask.time)
                 heapq.heappush(self.asks, new_ask)
 
-            trades.append(CDAMarket.Trade(top_ask.agent_id,
-                                          top_bid.agent_id, traded_quantity))
+            trades.append(CallTrade(top_ask.agent_id,
+                                    top_bid.agent_id, traded_quantity))
             midpoint_price = (top_bid.price + top_ask.price) / 2
 
         for trade in trades:
             self.agents[trade.buyer_id].buy(midpoint_price, trade.quantity)
             self.agents[trade.seller_id].sell(midpoint_price, trade.quantity)
+
+
+class CDAMarket:
+    def __init__(self, agents: List[Agent]) -> None:
+        self.agents = agents
+        self.bids: List[BookBid] = []
+        self.asks: List[BookAsk] = []
+        self._time: int = 0
+
+    @property
+    def time(self):
+        t = self._time
+        self._time += 1
+        return t
+
+    def run(self):
+        for agent in self.agents:
+            bids = [BookBid(b.quantity, b.price, i, self.time)
+                    for i, b in enumerate(agent.bid())]
+            for bid in bids:
+                heapq.heappush(self.bids, bid)
+
+            asks = [BookAsk(b.quantity, b.price, i, self.time)
+                    for i, b in enumerate(agent.ask())]
+            for ask in asks:
+                heapq.heappush(self.asks, ask)
+
+        trades: List[CDATrade] = []
+        while len(self.bids) != 0 and len(self.asks) != 0:
+            top_bid = self.bids[0]
+            top_ask = self.asks[0]
+            if top_bid.price < top_ask.price:
+                break
+
+            heapq.heappop(self.bids)
+            heapq.heappop(self.asks)
+            traded_quantity = abs(top_bid.quantity - top_ask.quantity)
+
+            if top_bid.quantity > top_ask.quantity:
+                new_bid = BookBid(
+                    top_bid.quantity - traded_quantity, top_bid.price, top_bid.agent_id, top_bid.time)
+                heapq.heappush(self.bids, new_bid)
+            if top_ask.quantity > top_bid.quantity:
+                new_ask = BookAsk(
+                    top_ask.quantity - traded_quantity, top_ask.price, top_ask.agent_id, top_ask.time)
+                heapq.heappush(self.asks, new_ask)
+
+            price = top_bid.price if top_bid.time < top_ask.time else top_ask.time
+            trades.append(CDATrade(top_ask.agent_id,
+                                   top_bid.agent_id, traded_quantity, price))
+
+        for trade in trades:
+            self.agents[trade.buyer_id].buy(trade.price, trade.quantity)
+            self.agents[trade.seller_id].sell(trade.price, trade.quantity)
