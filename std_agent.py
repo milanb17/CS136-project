847f73acd303ba71daa3f62fb869fe8ed68ffe19
zevbo <zevminskyprimus@gmail.com>
2021-12-05 13:29:21 -0500

    fix

--- std_agent.py
+++ std_agent.py
@@ -6,13 +6,17 @@ class StdAgent(Agent):
 
     def total_utility(self, market_data: MarketData, num_credits: int):
         carbon_free_q: int = math.floor(
-            self.marginal_utility_inv(market_data.carbon_price))
+            self.demand_inv(market_data.carbon_p))
         only_carbon: bool = carbon_free_q < num_credits
         quantity: int = carbon_free_q if only_carbon else math.floor(
-            self.marginal_utility_inv(market_data.renewable_price))
-        cost: float = quantity * market_data.carbon_price if only_carbon else num_credits * \
-            market_data.carbon_price + \
-            (quantity - num_credits) * market_data.renewable_price
+            self.demand_inv(market_data.renew_p))
+        cost: float = quantity * market_data.carbon_p if only_carbon else num_credits * \
+            market_data.carbon_p + \
+            (quantity - num_credits) * market_data.renew_p
+        return self.util(quantity) - cost
 
     def bid(self, market_data: MarketData) -> List[AgentBid]:
-        return AgentBid(1, self.utility)
+        q_buy = 1
+        curr_util = self.total_utility(market_data, self.num_credits)
+        on_buy_util = self.total_utility(market_data, self.num_credits + q_buy)
+        return AgentBid(q_buy, on_buy_util - curr_util)
