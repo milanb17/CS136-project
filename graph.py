0aaeeabe721852c9fc8459449aaa2453bd8ea786
Milan Bhandari <milanbhandari@college.harvard.edu>
2021-12-05 23:46:37 -0500

    generated some graphs

--- graph.py
+++ graph.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 import os
 
 csvs = ["innovation_bump.csv", "prosection.csv",
@@ -8,11 +7,14 @@ csvs = ["innovation_bump.csv", "prosection.csv",
 for csv in csvs:
     df = pd.read_csv(os.path.join(os.curdir, csv))
     varying_parameter = df.iloc[:, 0]
+    xlabel = df.columns[0]
 
     for i in [2, 3, 4]:
         y = df.iloc[:, i]
+        ylabel = df.columns[i]
 
         plt.plot(varying_parameter, y)
-        plt.xlabel(df.columns[0])
-        plt.ylabel(df.columns[i])
-        plt.show()
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.savefig(f"./plots/{xlabel}-{ylabel}.png")
+        plt.close()
